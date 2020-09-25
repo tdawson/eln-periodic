3e533a00bd4824a1d412a13f5d357e6d2aeccba0
Troy Dawson <tdawson@redhat.com>
2020-09-25 12:59:07 -0700

    create buildable list

--- eln-check.py
+++ eln-check.py
@@ -189,6 +189,12 @@ if __name__ == "__main__":
     overall_packagelist = get_distro_packages()
     eln_builds = get_eln_builds()
 
+    # Create the buildable list
+    with open("buildable-eln-packages.txt", 'w') as b:
+        for package_name in overall_packagelist:
+            if package_name not is_on_hold(package_name) and not is_excluded(package_name):
+                b.write("{0}\n".format(package_name))
+
     f = open(args.output, 'w')
     s = open(args.status, 'w')
     u = open(args.untag, 'w')
@@ -303,7 +309,7 @@ if __name__ == "__main__":
         else:
             this_package['color'] = color_none
             counter_none += 1
-            package_list.append(this_package)
+        package_list.append(this_package)
     counter_total = counter_same + counter_old + counter_none
     if counter_total == 0:
         percentage_same = "?%"
